58324a6e075a01237926312387137f8e30a3af87
topcueser <topcueser@gmail.com>
2022-06-18 17:08:12 +0300

    microservice init

---
+++ PhotoAppEurekaService/src/test/java/com/topcueser/photoapp/eureka/PhotoAppEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topcueser.photoapp.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
