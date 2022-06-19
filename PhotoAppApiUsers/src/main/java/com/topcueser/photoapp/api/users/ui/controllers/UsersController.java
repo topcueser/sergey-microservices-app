ad840e0da5e5981c5b916ba6459311fc787fd9f9
topcueser <topcueser@gmail.com>
2022-06-19 23:38:56 +0300

    users microservices add mysql8

--- PhotoAppApiUsers/src/main/java/com/topcueser/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/topcueser/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.topcueser.photoapp.api.users.ui.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UsersController {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping("/status/check")
     public String status() {
-        return "User Service Working";
+        return "User Service Working on port: " + environment.getProperty("local.server.port");
     }
 }
--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 
 <b>Netflix Eureka Server</b>
 ![Eureka Server](https://github.com/topcueser/sergey-microservices-app/blob/master/image/netflix-eureka.png?raw=true)
+
 <br>
+
 <b>Zuul Api Gateway</b>
 ![Zuul Api Gateway](https://github.com/topcueser/sergey-microservices-app/blob/master/image/zuul-api-gateway.png?raw=true)
 ![Zuul Api Gateway](https://github.com/topcueser/sergey-microservices-app/blob/master/image/Zuul_Api_Gateway.png?raw=true)
+
 <br>
+
 <b>Spring Cloud API Gateway - as another example</b>
 ![Spring Cloud API Gateway](https://github.com/topcueser/sergey-microservices-app/blob/master/image/spring-cloud-api-gateway.png?raw=true)
+
 <br>
+
+Http Request -> Zuul-Server -> Eureka-Server -> User-Service
