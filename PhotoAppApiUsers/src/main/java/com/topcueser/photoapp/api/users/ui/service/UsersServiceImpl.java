62cd6548aa9d8da0f963e12b14425b632790152b
topcueser <topcueser@gmail.com>
2022-06-22 12:23:33 +0300

    update versions

--- PhotoAppApiUsers/src/main/java/com/topcueser/photoapp/api/users/ui/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/topcueser/photoapp/api/users/ui/service/UsersServiceImpl.java
@@ -5,7 +5,6 @@ import com.topcueser.photoapp.api.users.data.UsersRepository;
 import com.topcueser.photoapp.api.users.shared.UserDto;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,7 +12,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -45,12 +43,9 @@ public class UsersServiceImpl implements UsersService{
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		UserEntity userEntity = findByEmailGetUser(username);
-		
 		if(userEntity == null) throw new UsernameNotFoundException(username);
-		
-		//List<SimpleGrantedAuthority> authorities = user.getRoles().stream().map(role -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());
-        //return new User(user.getUsername(), user.getPassword(), authorities);
-		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), true, true, true, true, new ArrayList<>());
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(),
+                true, true, true, true, new ArrayList<>());
 	}
 
 	@Override
