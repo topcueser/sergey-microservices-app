a8626b28d419f2f2dc07d5246f30e357cbd28c6b
topcueser <topcueser@gmail.com>
2022-08-19 01:19:38 +0300

    spring cloud api gateway - jwt authorization

--- SpringCloudApiGateway/src/main/java/com/topcueser/photoapp/api/gateway/AuthorizationHeaderFilter.java
+++ SpringCloudApiGateway/src/main/java/com/topcueser/photoapp/api/gateway/AuthorizationHeaderFilter.java
@@ -1,2 +1,75 @@
-package com.topcueser.photoapp.api.gateway;public class AuthorizationHeaderFilter {
+package com.topcueser.photoapp.api.gateway;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Objects;
+
+@Component
+public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
+
+    @Autowired
+    Environment environment;
+
+    public AuthorizationHeaderFilter() {
+        super(Config.class);
+    }
+
+    public static class Config {
+
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+
+            ServerHttpRequest request = exchange.getRequest();
+
+            if(!request.getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                return onError(exchange, "No authorization header", HttpStatus.UNAUTHORIZED);
+            }
+
+            String authorizationHeader = Objects.requireNonNull(request.getHeaders().get(HttpHeaders.AUTHORIZATION)).get(0);
+            String jwtToken = authorizationHeader.replace("Bearer ", "");
+
+            if(!isJwtValid(jwtToken)) {
+                return onError(exchange, "JWT token is not valid", HttpStatus.UNAUTHORIZED);
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String err, HttpStatus httpStatus) {
+        ServerHttpResponse response = exchange.getResponse();
+        response.setStatusCode(httpStatus);
+        return response.setComplete();
+    }
+
+    private boolean isJwtValid(String jwtToken) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(Objects.requireNonNull(environment.getProperty("jwt.token.secret")));
+            DecodedJWT decodedJWT = JWT.require(algorithm)
+                    .build()
+                    .verify(jwtToken);
+            String subject = decodedJWT.getSubject();
+            return subject != null && !subject.isEmpty();
+        } catch (Exception exception) {
+            return false;
+        }
+    }
 }
--- SpringCloudApiGateway/src/main/java/com/topcueser/photoapp/api/gateway/MyPreFilter.java
+++ SpringCloudApiGateway/src/main/java/com/topcueser/photoapp/api/gateway/MyPreFilter.java
@@ -1,2 +1,36 @@
-package com.topcueser.photoapp.api.gateway;public class MyPreFilter {
+package com.topcueser.photoapp.api.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Set;
+
+@Component
+public class MyPreFilter implements GlobalFilter {
+
+    final Logger logger = LoggerFactory.getLogger(MyPreFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("My first Pre-filter is executed...");
+
+        String requestPath = exchange.getRequest().toString();
+        logger.info("Request path = " + requestPath);
+
+        HttpHeaders headers = exchange.getRequest().getHeaders();
+        Set<String> headerNames = headers.keySet();
+
+        headerNames.forEach((headerName) -> {
+            String headerValue = headers.getFirst(headerName);
+            logger.info(headerName + " " + headerValue);
+        });
+
+        return chain.filter(exchange);
+    }
 }
