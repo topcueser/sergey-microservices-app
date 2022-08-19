3ded60a9022c134f99947334294029ded5c466e1
topcueser <topcueser@gmail.com>
2022-08-19 12:45:54 +0300

    spring cloud api gateway - global filter order

--- SpringCloudApiGateway/src/main/java/com/topcueser/photoapp/api/gateway/GlobalFiltersConfiguration.java
+++ SpringCloudApiGateway/src/main/java/com/topcueser/photoapp/api/gateway/GlobalFiltersConfiguration.java
@@ -5,38 +5,43 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class GlobalFiltersConfiguration {
+
     final Logger logger = LoggerFactory.getLogger(GlobalFiltersConfiguration.class);
 
+    @Order(1)
     @Bean
     public GlobalFilter secondPreFilter() {
         return (exchange, chain) -> {
             logger.info("My second global pre-filter is executed...");
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                logger.info("My second global post-filter was executed...");
+                logger.info("[3]My third global pre-filter is executed...");
             }));
         };
     }
 
+    @Order(2)
     @Bean
     public GlobalFilter thirdPreFilter() {
         return (exchange, chain) -> {
-            logger.info("My second global pre-filter is executed...");
+            logger.info("My third global pre-filter is executed...");
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                logger.info("My third global post-filter was executed...");
+                logger.info("[2]My second global pre-filter is executed...");
             }));
         };
     }
 
+    @Order(3)
     @Bean
     public GlobalFilter fourthPreFilter() {
         return (exchange, chain) -> {
-            logger.info("My second global pre-filter is executed...");
+            logger.info("My fourth global pre-filter is executed...");
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                logger.info("My fourth global post-filter was executed...");
+                logger.info("[1]My first global pre-filter is executed...");
             }));
         };
     }
--- SpringCloudApiGateway/src/main/java/com/topcueser/photoapp/api/gateway/MyPostFilter.java
+++ SpringCloudApiGateway/src/main/java/com/topcueser/photoapp/api/gateway/MyPostFilter.java
@@ -4,12 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 @Component
-public class MyPostFilter implements GlobalFilter {
+public class MyPostFilter implements GlobalFilter, Ordered {
 
     final Logger logger = LoggerFactory.getLogger(MyPostFilter.class);
 
@@ -17,7 +18,12 @@ public class MyPostFilter implements GlobalFilter {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            logger.info("Global Post-filter executed...");
+            logger.info("My last post filter is executed...");
         }));
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
     }
 }
